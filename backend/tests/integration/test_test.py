d5eaaec52de44af44fb5257af48a8a8ae9548f97
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-12-04 15:24:02 +0100

    workflows: enable continue-on-error for OctoCov action; remove unused add function and related tests

--- backend/tests/integration/test_test.py
+++
@@ -1,5 +0,0 @@
-from integration._marker import pytestmark  # noqa: F401
-
-
-def test_add():
-    assert 3 == 3
--- backend/tests/unit/test_test.py
+++
@@ -1,7 +0,0 @@
-from add import add
-
-from ._marker import pytestmark  # noqa: F401
-
-
-def test_add():
-    assert add(1, 2) == 3
