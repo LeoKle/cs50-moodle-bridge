77beb761eb2cdb3fdc1b902bca6b31d9668cdf71
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-12-11 20:10:37 +0100

    move repository interface to interfaces/repositories & use generic type

---
+++ backend/src/interfaces/repositories/repository_interface.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+class Repository[T](ABC):
+    """Generic repository interface for CRUD operations."""
+
+    @abstractmethod
+    def create(self, data: T) -> T:
+        pass
+
+    @abstractmethod
+    def get(self, item_id: str) -> T | None:
+        pass
+
+    @abstractmethod
+    def get_all(self) -> list[T]:
+        pass
+
+    @abstractmethod
+    def update(self, item_id: str, data: T) -> T | None:
+        pass
+
+    @abstractmethod
+    def delete(self, item_id: str) -> bool:
+        pass
