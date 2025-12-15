cb00863dd0059f6d3857062374d31b48588e8c76
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-12-15 17:07:55 +0100

    fix(MockCourseService): remove DTOs use domain model

--- backend/tests/mocks/services/course_service_mock.py
+++ backend/tests/mocks/services/course_service_mock.py
@@ -1,24 +1,24 @@
-from api.models.course import CourseCreate, CourseOut, CourseUpdate
 from interfaces.services.course_service import ICourseService
+from models.course import Course
 
 
 class MockCourseService(ICourseService):
     def get_course(self, course_id):
-        return CourseOut(id=course_id, name="Mock Course", cs50_id=50, exercise_ids=[])
+        return Course(id=course_id, name="Mock Course", cs50_id=50, exercise_ids=[])
 
     def get_courses(self):
         return [
-            CourseOut(id="1", name="Course 1", cs50_id=50, exercise_ids=[]),
-            CourseOut(id="2", name="Course 2", cs50_id=51, exercise_ids=[]),
+            Course(id="1", name="Course 1", cs50_id=50, exercise_ids=[]),
+            Course(id="2", name="Course 2", cs50_id=51, exercise_ids=[]),
         ]
 
-    def create_course(self, data: CourseCreate):
-        return CourseOut(
+    def create_course(self, data: Course):
+        return Course(
             id="new_id", name=data.name, cs50_id=data.cs50_id, exercise_ids=data.exercise_ids
         )
 
-    def update_course(self, course_id: str, data: CourseUpdate):
-        return CourseOut(
+    def update_course(self, course_id: str, data: Course):
+        return Course(
             id=course_id,
             name=data.name,
             cs50_id=data.cs50_id or 50,
